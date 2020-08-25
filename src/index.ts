87b0f4a7fc0024f9842bc031d20088a17e44af05
Drew Stone <drewstone329@gmail.com>
2020-08-25 12:36:38 -0400

    Dev fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Mainnet } from './spec/mainnet';
 export { default as Beresheet } from './spec/mainnet';
+export { default as dev } from './spec/dev';
 export * from './interfaces/index';
