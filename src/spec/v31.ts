3c176091290977a3d66ad04840069eb3b46bd21c
Jake Naviasky <jake@commonwealth.im>
2021-01-26 16:04:48 -0500

    Fix address type bug.

--- src/spec/v31.ts
+++ src/spec/v31.ts
@@ -2,56 +2,56 @@ import { RegistryTypes, OverrideModuleType } from '@polkadot/types/types';
 
 const types: RegistryTypes = {
   // old EDG types
-  "ProposalRecord": {
-    "index": "u32",
-    "author": "AccountId",
-    "stage": "VoteStage",
-    "transition_time": "u32",
-    "title": "Text",
-    "contents": "Text",
-    "vote_id": "u64"
+  'ProposalRecord': {
+    'index': 'u32',
+    'author': 'AccountId',
+    'stage': 'VoteStage',
+    'transition_time': 'u32',
+    'title': 'Text',
+    'contents': 'Text',
+    'vote_id': 'u64'
   },
-  "ProposalContents": "Bytes",
-  "ProposalTitle": "Bytes",
-  "Balance2": "u128",
-  "VoteStage": {
-    "_enum": [
-      "PreVoting",
-      "Commit",
-      "Voting",
-      "Completed"
+  'ProposalContents': 'Bytes',
+  'ProposalTitle': 'Bytes',
+  'Balance2': 'u128',
+  'VoteStage': {
+    '_enum': [
+      'PreVoting',
+      'Commit',
+      'Voting',
+      'Completed'
     ]
   },
-  "VoteType": {
-    "_enum": [
-      "Binary",
-      "MultiOption",
-      "RankedChoice"
+  'VoteType': {
+    '_enum': [
+      'Binary',
+      'MultiOption',
+      'RankedChoice'
     ]
   },
-  "TallyType": {
-    "_enum": [
-      "OnePerson",
-      "OneCoin"
+  'TallyType': {
+    '_enum': [
+      'OnePerson',
+      'OneCoin'
     ]
   },
-  "VoteOutcome": "[u8; 32]",
-  "VotingTally": "Option<Vec<(VoteOutcome, u128)>>",
-  "VoteData": {
-    "initiator": "AccountId",
-    "stage": "VoteStage",
-    "vote_type": "VoteType",
-    "tally_type": "TallyType",
-    "is_commit_reveal": "bool"
+  'VoteOutcome': '[u8; 32]',
+  'VotingTally': 'Option<Vec<(VoteOutcome, u128)>>',
+  'VoteData': {
+    'initiator': 'AccountId',
+    'stage': 'VoteStage',
+    'vote_type': 'VoteType',
+    'tally_type': 'TallyType',
+    'is_commit_reveal': 'bool'
   },
-  "Commitments": "Vec<(AccountId, VoteOutcome)>",
-  "Reveals": "Vec<(AccountId, Vec<VoteOutcome>)>",
-  "VoteRecord": {
-    "id": "u64",
-    "commitments": "Commitments",
-    "reveals": "Reveals",
-    "data": "VoteData",
-    "outcomes": "Vec<VoteOutcome>"
+  'Commitments': 'Vec<(AccountId, VoteOutcome)>',
+  'Reveals': 'Vec<(AccountId, Vec<VoteOutcome>)>',
+  'VoteRecord': {
+    'id': 'u64',
+    'commitments': 'Commitments',
+    'reveals': 'Reveals',
+    'data': 'VoteData',
+    'outcomes': 'Vec<VoteOutcome>'
   },
 
   // aliases that don't do well as part of interfaces
@@ -72,8 +72,8 @@ const types: RegistryTypes = {
   RewardDestination: 'RewardDestinationTo257',
   RefCount: 'RefCountTo259',
   AccountInfo: 'AccountInfoWithRefCount',
-  Address: 'AccountId',
-  LookupSource: 'AccountId',
+  Address: 'IndicesLookupSource',
+  LookupSource: 'IndicesLookupSource',
 };
 
 const typesAlias: Record<string, OverrideModuleType> = {
